b47fa9cd53660121bf54cfb160340b6cf531bdaa
H-Neethika <neethikahariharasakthy2000@gmail.com>
2025-10-10 11:51:52 +0530

    chore: init microservice architecture structure

---
+++ Backend/exhibition-service/src/main/java/com/exhibition/exhibition_service/ExhibitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exhibition.exhibition_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/exhibition-service/src/test/java/com/exhibition/exhibition_service/ExhibitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exhibition.exhibition_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
