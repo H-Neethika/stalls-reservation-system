af4b7a8f420ca08fad6e067ea38cac4514b56b42
H-Neethika <neethikahariharasakthy2000@gmail.com>
2025-11-12 12:10:34 +0530

    feat(booking-service): update ExhibitionStall entity mapping to support multiple stalls per stall type

--- Backend/booking-service/src/main/java/com/booking/booking_service/model/ExhibitionStall.java
+++ Backend/booking-service/src/main/java/com/booking/booking_service/model/ExhibitionStall.java
@@ -26,7 +26,7 @@ public class ExhibitionStall {
     private String stallName;
 
 
-    @OneToOne
+    @ManyToOne
     private StallType stallType;
 
 
--- Backend/booking-service/src/main/java/com/booking/booking_service/repository/StallTypeRepository.java
+++ Backend/booking-service/src/main/java/com/booking/booking_service/repository/StallTypeRepository.java
@@ -4,9 +4,10 @@ import com.booking.booking_service.model.ExhibitionHall;
 import com.booking.booking_service.model.StallType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StallTypeRepository extends JpaRepository <StallType,Long> {
-    Optional<StallType> findByExhibitionHallId(ExhibitionHall exhibitionHall);
+    List<StallType> findByExhibitionHallId(ExhibitionHall exhibitionHall);
 
 }
