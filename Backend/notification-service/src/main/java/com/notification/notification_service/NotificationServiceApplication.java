30516f7de86e60d037beb6b5c4fc5ae6c6d6661b
JaanuGopan <sjanugopan@gmail.com>
2025-11-15 11:38:26 +0530

    feat(notification): add asynchronous email processing configuration and update email notification handling

--- Backend/notification-service/src/main/java/com/notification/notification_service/NotificationServiceApplication.java
+++ Backend/notification-service/src/main/java/com/notification/notification_service/NotificationServiceApplication.java
@@ -2,10 +2,8 @@ package com.notification.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
